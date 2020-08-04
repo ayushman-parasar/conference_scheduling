9cc97ac8f35b2785e1672117b59aafab7873446d
Ayush_Mann <learnertsk@gmail.com>
2020-08-05 00:25:22 +0530

    morning talks for track2

--- sessions.rb
+++ sessions.rb
@@ -1,15 +1,21 @@
 require_relative "talk"
 class Sessions
   def initialize
-    @morning_talks1 = []
-    @available_morning_mins_for_track1 = 180
     @array = Talk.list
+    @morning_talks1 = []
+    @morning_talks2 = []
     @call = 1
+    @available_morning_mins_for_track2 = @available_morning_mins_for_track1 = 180
+    @batch_for_eveningtalks = [] 
+    @evening_talks1 = []
+    @evening_talks2 = []
     @call_evening = 1
+    @available_evening_mins_for_track1 = @available_evening_mins_for_track2 = 240
+       
   end
 
   def adding_programs
-    @array.each do |talk|
+    Talk.list.each do |talk|
       talk.each do |key, value|
         if @available_morning_mins_for_track1 == 0
           break
@@ -19,7 +25,6 @@ class Sessions
         end
       end
     end
-    p "morning session for track1 #{@morning_talks1}"
     remove_over_talks(@morning_talks1)
   end
 
@@ -37,10 +42,28 @@ class Sessions
     @array = @array - result
     if @call == 1
       @call += 1
-      p @array, "after removing"
+      adding_morning_talks_track2(@array)
     else
       @array
     end
+  end
+  
+  def adding_morning_talks_track2(array_of_hsh)
+    array_of_hsh.each do |hsh|
+      hsh.each do |key, value|
+        if @available_morning_mins_for_track2 == 0
+          break
+        else
+          @morning_talks2 << {key=>value}
+          @available_morning_mins_for_track2 = @available_morning_mins_for_track2.to_i -  value.to_i
+          break
+        end
+      end
+    end
+    @batch_for_eveningtalks =  remove_over_talks(@morning_talks2)
+    p @batch_for_eveningtalks, "evenning talks" 
+
+    
   end
 end
 
