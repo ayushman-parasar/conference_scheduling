01f25705aa2dd8c22170df214a4d50701d58ab48
Ayush_Mann <learnertsk@gmail.com>
2020-08-05 00:18:11 +0530

    function to remove added talks

--- sessions.rb
+++ sessions.rb
@@ -3,11 +3,13 @@ class Sessions
   def initialize
     @morning_talks1 = []
     @available_morning_mins_for_track1 = 180
-
+    @array = Talk.list
+    @call = 1
+    @call_evening = 1
   end
 
   def adding_programs
-    Talk.list.each do |talk|
+    @array.each do |talk|
       talk.each do |key, value|
         if @available_morning_mins_for_track1 == 0
           break
@@ -18,6 +20,27 @@ class Sessions
       end
     end
     p "morning session for track1 #{@morning_talks1}"
+    remove_over_talks(@morning_talks1)
+  end
+
+  def remove_over_talks(items)
+    result = []
+    @array.each do |hsh|
+      hsh.each do |key, value|
+        for hass in items
+          if hass[key]
+            result << hass
+          end
+        end
+      end
+    end
+    @array = @array - result
+    if @call == 1
+      @call += 1
+      p @array, "after removing"
+    else
+      @array
+    end
   end
 end
 
